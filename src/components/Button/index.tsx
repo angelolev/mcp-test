51202612468654ff3e2e9e717fe66c41fd9a7ffa
Angelo Leva <angelokta7@gmail.com>
2025-09-08 22:17:59 -0500

    fix(button): change label to 'Soy un boton' (#MBA-11)

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./Button.module.css";
 
 function Button() {
-  return <button className={styles.button}>Click me</button>;
+  return <button className={styles.button}>Soy un boton</button>;
 }
 
 export default Button;
